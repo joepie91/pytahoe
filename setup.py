2e432dfa619c452e867308ea7f608599e2a869b4
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-07-07 23:53:00 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytahoe',
-      version='1.0.2',
+      version='1.0.3',
       description='Python module for working with the Tahoe-LAFS filesystem.',
       long_description='''
 	This module allows for easy interaction with a Tahoe-LAFS grid, via the WebAPI.
